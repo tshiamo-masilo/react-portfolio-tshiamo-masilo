22bd248ebe8399a20bb2ec8e0ac0365dbeee9fd6
tshiamo-masilo <63866003+tshiamo-masilo@users.noreply.github.com>
2023-05-04 20:23:52 +0200

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section></section>
+    </main>
+  )
+}
